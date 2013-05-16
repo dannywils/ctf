bcfb6643fe02d9cb4036d1975e6a7902b5c101f8
Danny Wilson <dannywils@gmail.com>
2013-05-16 15:48:39 -0300

    Plot other user coords. Add css file.

--- js/main.js
+++ js/main.js
@@ -18,27 +18,35 @@ $('document').ready(function () {
 	db.select("users", {
 		username: username
 	}, function(data) {
+		//if the user doesn't exists, add them and refresh
 		if (data.length == 0) {
 			db.insert("users", {username: username,	team: 1	}, function(data){
-				user = [data];
-				$('p.result').html(data.username);
-				domap();
+				document.location = '';
 			});
+		//otherwise show the map
 		} else {
 			$('p.result').html('Username: '+data[0].username);
 			user = data;
-			domap();
+			showMap();
 		}
 	});
 
-	function domap(){
-		var map = new mapper();
-
+	function showMap(){
 		watchLocation(function (coords) {
+			var map = new mapper();
 			$('p.coords').html('Location: ' + coords.latitude + ',' + coords.longitude);
 			var locate = [coords.latitude + ',' + coords.longitude];
-			map.plotLocation(locate);
-			db.update('users', user[0]._id.$oid, { location: locate[0] }, function(data){ });
+			//send your location to the server
+			db.update('users', user[0]._id.$oid, { location: locate[0], date: new Date().toISOString() }, function(data){ });
+			//get the other users
+			db.select('users',{ "username": { "$ne" : user[0].username }},function(data){
+				//plot the other users
+				for (var i = data.length - 1; i >= 0; i--) {
+					map.plotLocation([data[i].location]);
+				};
+				//plot yourself
+				map.plotLocation(locate);
+			});
 			console.log('Sent update to server.');
 		}, function () {
 			$('p.coords').html('error');
